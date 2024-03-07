aacd84548d5b2fec295f0570c8a327ade9d101e7
mostGraciousDevotee <faikar041289@gmail.com>
2024-03-07 19:29:17 +0700

    Refactoring Main

--- Immortal Test/Assets/Scripts/Main/Main.cs
+++ Immortal Test/Assets/Scripts/Main/Main.cs
@@ -6,14 +6,13 @@ using Immortal.CommandFactoryPackage;
 using Immortal.CommandImplementation;
 
 using Immortal.UnitImplementation;
+using Immortal.UnitFactoryPackage;
 
 using Immortal.CellFactoryPackage;
 using Immortal.CellImplementation;
 
 using Immortal.EngineImplementation;
 using Immortal.SceneImplementation;
-
-using Immortal.PresenterFactory;
 
 using Immortal.GameSystem;
 using Immortal.GameImplementation;
@@ -29,14 +28,37 @@ namespace Immortal.Main
 {
     public class Main : MonoBehaviour
     {
+        // BattleSceneDependencies
+        IUnitFactory _unitFactory;
+        ICellFactory _cellFactory;
+        IDisplayRangeResponderFactory _displayRangeResponderFactory;
+        ICommandHistory _commandHistory;
+        
         void Awake()
         {
-            InitMainCommandFactory();
-
-            InitActionCommandFactory();
+            BuildMainSceneDependencies();
+            BuildBattleSceneDependencies();
         }
 
-        void InitMainCommandFactory()
+        void BuildBattleSceneDependencies()
+        {
+            CreateUnitFactory();
+            CreateCellFactory();
+            CreateDisplayRangeResponderFactory();
+            CreateCommandHistory();
+
+            // DisplayRange
+            InitDisplayRangeResponderFactory();
+            InitCommandHistory();
+            InitActionCommandFactory();
+
+            // UnitPosition
+            InitCellFactory();
+            InitGameBuilder();
+            InitGame();
+        }
+
+        void BuildMainSceneDependencies()
         {
             var sceneLoader = new SceneLoader();
             var unityApp = new UnityApp();
@@ -46,49 +68,75 @@ namespace Immortal.Main
 
         void InitActionCommandFactory()
         {
-            var unitFactory = new UnitFactory();
-            var cellFactory = new CellFactory();
-            var cellDisplayContainer = GetComponent<ICellDisplayPrefabs>();
-
-            var displayRangeResponderFactory = new DisplayRangeResponderFactory
-            (
-                unitFactory.MakeTurnManager(),
-                cellFactory.GetSquareCells(),
-                cellFactory.GetValidMoveProvider(),
-                cellFactory.GetValidAttackProvider()
-            );
-
-            var commandHistory = new CommandHistory();
-
-            Singleton<ICommandHistory>.Instance = commandHistory;
-
-            var game = new Game
-            (
-                unitFactory.MakeTurnManager()
-            );
-
             var actionCommandFactory = new ActionCommandFactory
             (
-                unitFactory,
-                displayRangeResponderFactory,
-                commandHistory
+                _unitFactory,
+                _displayRangeResponderFactory,
+                _commandHistory
             );
-
-            Singleton<IDisplayRangeResponderFactory>.Instance = displayRangeResponderFactory;
             Singleton<IActionCommandFactory>.Instance = actionCommandFactory;
+        }
 
-            // TODO : IGame should have an event
-            // DisplayMoveCells
-            // DisplayAttackCells
+        void InitDisplayRangeResponderFactory()
+        {
+            Singleton<IDisplayRangeResponderFactory>.Instance = _displayRangeResponderFactory;
+        }
+
+        void CreateUnitFactory()
+        {
+            _unitFactory = new UnitFactory();
+        }
+
+        void CreateCellFactory()
+        {
+            _cellFactory = new CellFactory();
+        }
+
+        void CreateDisplayRangeResponderFactory()
+        {
+            _displayRangeResponderFactory = new DisplayRangeResponderFactory
+            (
+                _unitFactory.MakeTurnManager(),
+                _cellFactory.GetSquareCells(),
+                _cellFactory.GetValidMoveProvider(),
+                _cellFactory.GetValidAttackProvider()
+            );
+        }
+
+        void CreateCommandHistory()
+        {
+            _commandHistory = new CommandHistory();
+        }
+
+        void InitCellFactory()
+        {
+            Singleton<ICellFactory>.Instance = _cellFactory;
+        }
+
+        void InitCommandHistory()
+        {
+            Singleton<ICommandHistory>.Instance = _commandHistory;
+        }
+
+        void InitGameBuilder()
+        {
             var gameBuilder = new GameBuilder
             (
-                unitFactory,
-                cellFactory,
-                commandHistory
+                _unitFactory,
+                _cellFactory,
+                _commandHistory
             );
 
-            Singleton<ICellFactory>.Instance = cellFactory;
             Singleton<IGameBuilder>.Instance = gameBuilder;
+        }
+
+        void InitGame()
+        {
+            var game = new Game
+            (
+                _unitFactory.MakeTurnManager()
+            );
+
             Singleton<IGame>.Instance = game;
         }
     }
